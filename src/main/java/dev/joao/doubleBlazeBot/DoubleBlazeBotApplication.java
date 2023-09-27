8fd3d8c64b3bda9e4924ea6a7106c1cfd33ef23c
Joao Henrique <joaoibitira@gmail.com>
2023-09-27 18:10:44 -0300

    first commit

---
+++ src/main/java/dev/joao/doubleBlazeBot/DoubleBlazeBotApplication.java
@@ -0,0 +1,13 @@
+package dev.joao.doubleBlazeBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleBlazeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleBlazeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joao/doubleBlazeBot/DoubleBlazeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.doubleBlazeBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleBlazeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
